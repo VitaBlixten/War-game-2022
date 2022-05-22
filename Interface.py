9f7b9f635f2810e2bdf879eb201ae7d3a65e92aa
VitaBlixten <erikmoreno03@gmail.com>
2022-05-22 18:46:50 +0200

    Added images and fuktions in Interface to close, open settings and play

--- Interface.py
+++ Interface.py
@@ -1,20 +1,30 @@
 from cmu_graphics import *
+from sys import exit
+
+Options = Image('Installningar.png',0,0,height=400,width=400,visible = False)
+back1 = Rect(20,347,85,33,fill=rgb(72,76,67), border='Black',visible = False)
+yes = Label('Back',63,363,size=25,fill=rgb(228,224,210),visible = False)
 
 ###Box
-back = Rect(0,0,400, 400, fill='white')
-box = Rect(120,200,160,50,fill=None, border='Black')
-plus1 = Rect(120,110,160,50,fill=None, border='Black')
-plus2 = Rect(120,290,160,50,fill=None, border='Black')
+back = Image('Startsida.png',0,0,height=400,width=400)
+box = Rect(148,197,105,43,fill=None, border='Black')
+plus1 = Rect(148,148,105,43,fill=None, border='Black')
+plus2 = Rect(148,296,105,43,fill=None, border='Black')
+plus3 = Rect(148,249,105,43,fill=None, border='Black')
+
+
+Pic = Group(
+
+)
 
 boxes = Group(
 back,
 box, 
 plus1, 
-plus2, 
-Label("War", 200,45,size=40),
-Label("Play", 200,133,size=30),
-Label("Help", 200,223,size=30),
-Label("Something", 200,313,size=30))
+plus2,
+plus3, 
+)
+
 
 def help():
     controls = "To move your troops:"
@@ -39,16 +49,35 @@ def help():
 def onMouseMove(mouseX,mouseY):
     if box.contains(mouseX,mouseY):
         box.fill = "Lightgrey"
+        box.opacity = 45
+
     elif plus1.contains(mouseX,mouseY):
         plus1.fill = "Lightgrey"
-    
+        plus1.opacity = 45
+
     elif plus2.contains(mouseX,mouseY):
         plus2.fill = "Lightgrey"
+        plus2.opacity = 45
+
+    elif plus3.contains(mouseX,mouseY):
+        plus3.fill = "Lightgrey"
+        plus3.opacity = 45
+
+    elif back1.contains(mouseX,mouseY):
+        back1.fill = "Lightgrey"
+        back1.opacity = 45
         
     else:
         box.fill = None
         plus1.fill = None
         plus2.fill = None
+        plus3.fill = None
+        back1.fill = rgb(72,76,67)
+        back1.opacity = 100
+        box.opacity = 100
+        plus1.opacity = 100
+        plus2.opacity = 100
+        plus3.opacity = 100
 
 def onMousePress(mouseX, mouseY):
     if plus1.contains(mouseX,mouseY):
@@ -56,5 +85,20 @@ def onMousePress(mouseX, mouseY):
     if box.contains(mouseX,mouseY):
         boxes.clear()
         help()
+    if plus3.contains(mouseX,mouseY):
+        Pic.add(
+            Options,
+            back1,
+            yes
+        )
+        Options.visible = True
+        back1.visible = True
+        yes.visible = True
+        Pic.toFront()
+    if back1.contains(mouseX,mouseY):
+        Pic.clear()                
+    if plus2.contains(mouseX,mouseY):
+        exit()
+
 
 cmu_graphics.run()
